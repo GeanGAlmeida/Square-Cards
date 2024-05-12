987f9a85481af131390cbf7de6a44d4f7079ceb2
Gean Almeida <gomesgean81@gmail.com>
2024-05-11 23:27:08 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.getElementsByClassName("fa-brands fa-youtube")[0].addEventListener("click", function(){
+    window.open("https://youtu.be/8I5UCTlMa34?si=VZbLcOnYhq3SznMA")
+})
+document.getElementsByClassName("fa-brands fa-youtube")[1].addEventListener("click", function(){
+    window.open("https://youtu.be/wtqbOFYivxw?si=-yAmySak1Arx1ZU8")
+})
+document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener("click", function(){
+    window.open("https://youtu.be/H5tuwAO-_Kg?si=V_eqtS73tUF3jRNd")
+})
+document.getElementsByClassName("article")[0].addEventListener("click", function(){
+    window.open("https://jsremotely.com/company/1319-prospect")
+})
+document.getElementsByClassName("article")[1].addEventListener("click", function(){
+    window.open("https://jsremotely.com/company/1319-prospect")
+})
+document.getElementsByClassName("article")[2].addEventListener("click", function(){
+    window.open("https://jsremotely.com/company/1319-prospect")
+})
